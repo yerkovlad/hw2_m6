4d156568b5f72ce9dbeaf64beea1fa62afe5ed93
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-10-28 10:28:49 +0300

    Create query_1.sql

---
+++ query_1.sql
@@ -0,0 +1,6 @@
+SELECT s.first_name, s.last_name, AVG(g.grade) AS avg_grade
+FROM students s
+JOIN grades g ON s.student_id = g.student_id
+GROUP BY s.first_name, s.last_name
+ORDER BY avg_grade DESC
+LIMIT 5;
